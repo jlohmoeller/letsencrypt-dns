60228a534504cf13ac2bb8b06dfb86e57a23c146
Johannes Lohmöller <jlohmoeller@users.noreply.github.com>
2019-09-06 12:33:26 +0200

    Update certonly.sh

--- scripts/certonly.sh
+++ scripts/certonly.sh
@@ -7,3 +7,15 @@ certbot certonly \
   --manual-auth-hook "/dns_helper.sh auth" \
   --manual-cleanup-hook "/dns_helper.sh cleanup" \
   -d "$1"
+
+mkdir -p /certs/combined
+
+for cert in $(ls /etc/letsencrypt/live) ; do
+  certpath=/etc/letsencrypt/live/$cert
+  if [ -d "$certpath" ]; then
+    cat $certpath/fullchain.pem $certpath/privkey.pem > /certs/combined/$cert.pem
+    cp $certpath/fullchain.pem /certs/$cert-chain.pem
+    cp $certpath/privkey.pem /certs/$cert-key.pem
+    cp $certpath/cert.pem /certs/$cert.pem
+  fi
+done
