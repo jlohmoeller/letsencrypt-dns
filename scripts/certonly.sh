66cde35efdf408fff6c3c3ba5cb9bb0df0cae3e7
Johannes Lohmöller <johannes.lohmoeller@rwth-aachen.de>
2019-09-28 18:31:39 +0200

    Bugfix

--- scripts/certonly.sh
+++ scripts/certonly.sh
@@ -4,8 +4,8 @@ certbot certonly \
   --manual \
   --manual-public-ip-logging-ok \
   --preferred-challenges=dns \
-  --manual-auth-hook "/dns_helper.sh auth" \
-  --manual-cleanup-hook "/dns_helper.sh cleanup" \
+  --manual-auth-hook "/scripts/dns_helper.sh auth" \
+  --manual-cleanup-hook "/scripts/dns_helper.sh cleanup" \
   -d "$1"
 
 mkdir -p /certs/combined
